cb86c7b0898a45aba843287a4460221dac6c2a9d
Pgwsunny <814424014@qq.com>
2019-08-21 15:08:17 +0800

    js

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let result =[];
+  for (let i = 0; i < collectionA.length; i++) {
+    for (let j = 0; j < objectB.value.length; j++) {
+      if(objectB.value[j] ===collectionA[i].key){
+          result.push(collectionA[i].key)
+      }   
+    }
+  }
+  return result;
 }
